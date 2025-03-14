05ea5850d16cc43eab81ee7e783f1f6d8086c9fb
Aiwenqiuyu <96851487+Aiwenqiuyu@users.noreply.github.com>
2025-03-14 11:31:51 +0800

    Add files via upload

---
+++ hextech/main.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import json
+from lolhelper import *
+from analysis import TeamAnalysis
+from mainwindow import HeroRecommender
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+def main():
+    # 初始化Hextech窗口
+    app = QApplication(sys.argv)
+    window = HeroRecommender()
+    window.show()
+    if checkProcessAlive("LeagueClient"):
+        try:
+            window.lol_helper_init()
+        except RuntimeError as e:
+            window.set_status_message(str(e), 'error', 5000)
+        except ValueError as e:
+            window.set_status_message(str(e), 'error', 5000)
+        except Exception as e:
+            window.set_status_message("程序初始化失败，请检查日志获取详细信息", 'error', 5000)
+    else:
+        #print("未检测到游戏进程，请先运行游戏")
+        window.set_status_message("未检测到游戏进程，请先运行游戏", 'error', 5000)
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
+
+
